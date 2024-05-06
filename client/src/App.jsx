2c4484308998b30c14c2de3b312a028d933817d9
rohan-bankar <rohanbankar011@gmail.com>
2024-05-06 14:21:50 +0530

    react basic structure

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-red-600 bg-yellow-100 p-4'>Welcome to JodiDari</h1>
+    </>
+  )
+}
+
+export default App
