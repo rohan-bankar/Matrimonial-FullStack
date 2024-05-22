f7c09041cf644c0d67dbe6c60c5f074e99e28389
rohan-bankar <rohanbankar011@gmail.com>
2024-05-22 14:17:22 +0530

    footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Footer = () => {
   return (
       <footer className='py-5 px-6 bg-orange-300'>
-        <span className='font-bold text-white'>Rohan Bankar</span>
+        Copyright © 2024 : 
+                        <a href="https://github.com/rohan-bankar/Matrimonial-FullStack/blob/main/LICENSE" className=" underline text-sky-800 hover:text-sky-500 ml-1">
+                             Rohan Bankar
+                        </a>
       </footer>
   )
 }
